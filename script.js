964582f597fcd808403f888b94596f5fd7a219ea
Somia Sohail Ansari <48806685+somia-ansari@users.noreply.github.com>
2019-07-25 17:54:56 +0500

    WIP- PW2

--- script.js
+++ script.js
@@ -152,6 +152,7 @@ console.log("PW1--7 Print names and phone number of students who have Ufone.");
 for (let i = 0; i < students.length; i++) {
     if (students[i].phoneNo.charAt(2) == "3") {
         console.log(students[i].name);
+        console.log("Number: "+students[i].phoneNo);
 
     }
 }
@@ -167,8 +168,8 @@ for (let i = 0; i < students.length; i++) {
         groupB.push(students[i].name);
     }
 }
-console.log("Group A: " + groupA);
-console.log("Group B: " + groupB);
+console.log("Group A: " + groupA.join(', '));
+console.log("Group B: " + groupB.join(', '));
 
 console.log("PW1--9 Print age of each student in the below format:");
 
@@ -203,3 +204,177 @@ for (let i = 0; i < students.length; i++) {
     }
 }
 console.log(students[studentIndex].name);
+let videos = [
+    {
+        title: "Photoshop tutorial",
+        lengthInMinutes: 70,
+        category: "Education",
+        uploadDate: new Date("07-22-2019"),
+        tags: "design, digital, photoshop, creativity",
+        features: ["Live", "360°", "HDR"],
+        viewCount: 4700,
+        rating: 4.3
+    },
+    {
+        title: "Episode # 01 - The Best Comedy Show",
+        lengthInMinutes: 19,
+        category: "Entertainment",
+        uploadDate: new Date("07-03-2019"),
+        tags: "comedy, funny",
+        features: ["Subtitles/CC", "3D", "HD"],
+        viewCount: 145615,
+        rating: 3.9
+    },
+    {
+        title: "How to use FOR EACH loop - tutorial by Tech Karo",
+        lengthInMinutes: 6,
+        category: "Education",
+        uploadDate: new Date("11-10-2018"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 4.5
+    }, {
+        title: "Guess The Number game - 23rd July Tech Karo [Urdu]",
+        lengthInMinutes: 55,
+        category: "Education",
+        uploadDate: new Date("05-07-2018"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD", "Creative Commons"],
+        viewCount: 14,
+        rating: 4.5
+    },
+    {
+        title: "#1 Functions (Javascript) - Web Development - Tech Karo [Urdu]",
+        lengthInMinutes: 6,
+        category: "Education",
+        uploadDate: new Date("11-10-2018"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 4.5
+    },
+    {
+        title: "How to Publish React / React Native Library [Urdu/Hindi]",
+        lengthInMinutes: 53,
+        category: "Science",
+        uploadDate: new Date("05-10-2011"),
+        tags: "react library, react native library, npm package, npm library, npx package, npx library",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 5.5
+    },
+    {
+        title: "VS Code - Backup or Sync Your Settings, Extensions and Theme [URDU/HINDI]",
+        lengthInMinutes: 46,
+        category: "Education",
+        uploadDate: new Date("11-10-2018"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 4557,
+        rating: 2.5
+    },
+    {
+        title: "Sync VSCode extension | Share everything from one pc to other.",
+        lengthInMinutes: 56,
+        category: "Education",
+        uploadDate: new Date("11-10-2018"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 3.5
+    },
+    {
+        title: "Using Git with VS Code and Github",
+        lengthInMinutes: 60,
+        category: "Education",
+        uploadDate: new Date("11-10-1998"),
+        tags: "vsCode, git, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 3.1
+    },
+    {
+        title: "VS Code Can Do That?! VS Code Tips and Tricks",
+        lengthInMinutes: 48,
+        category: "Education",
+        uploadDate: new Date("11-10-2019"),
+        tags: "VS Code, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 4.5
+    },
+    {
+        title: "C++Now 2018: Rong Lu “C++ Development with Visual Studio Code”",
+        lengthInMinutes: 22,
+        category: "Web Development",
+        uploadDate: new Date("11-10-2004"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 9004,
+        rating: 2.4
+    },
+    {
+        title: "REST API concepts and examples",
+        lengthInMinutes: 44,
+        category: "Computer Sciences",
+        uploadDate: new Date("11-10-2005"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 1008,
+        rating: 1.5
+    },
+    {
+        title: "Best laptops for programming? How to get a job at Google? - And other FAQ’s!",
+        lengthInMinutes: 25,
+        category: "Education",
+        uploadDate: new Date("11-10-1999"),
+        tags: "javascript, loops, web development",
+        features: ["Purchased", "HD"],
+        viewCount: 8854,
+        rating: 2.2
+    },
+
+];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+console.log("PW2--1 Print each video in this format:");
+for (let i = 0; i < videos.length; i++) {
+    let fullDate = videos[i].uploadDate;
+
+    console.log("Title: " +
+        videos[i].title +
+        "\nLength: " +
+        videos[i].lengthInMinutes +
+        " minutes\nCategory: " +
+        videos[i].category +
+        "\nViews: " +
+        videos[i].viewCount.toLocaleString() +
+        "\n"+
+        fullDate.getDate() +
+        "-" + months[fullDate.getMonth()] +
+        "-" + fullDate.getFullYear() +
+        "\nRating: " +
+        videos[i].rating
+    );
+
+}
+console.log("PW2--2 Print titles of all short ( less than 3 minutes ) videos.");
+
+for (let i = 0; i < videos.length; i++) {
+    
+
+}
